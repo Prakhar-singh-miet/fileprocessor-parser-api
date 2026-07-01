bf2b3975779b0218a1a7e74a0169c9fd475a94d2
Prakhar-singh-miet <singhprakhar9758@gmail.com>
2026-07-01 12:05:17 +0530

    initial commit :spring boot project set up

---
+++ src/main/java/com/example/parserapi/ParserapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.parserapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParserapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParserapiApplication.class, args);
+	}
+
+}
