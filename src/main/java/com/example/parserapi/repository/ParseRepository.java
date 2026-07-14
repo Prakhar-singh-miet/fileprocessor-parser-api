9491cad45e1e114f0b6e50eb3aaa03d03e6ec0bc
Prakhar-singh-miet <singhprakhar9758@gmail.com>
2026-07-14 09:50:34 +0530

    added parseentity and parserepository

--- src/main/java/com/example/parserapi/repository/ParseRepository.java
+++ src/main/java/com/example/parserapi/repository/ParseRepository.java
@@ -1,8 +1,9 @@
 package com.example.parserapi.repository;
 
 
+import com.example.parserapi.entity.ParseEntity;
 import com.example.parserapi.entity.ParserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ParseRepository extends JpaRepository<ParserEntity,Long> {
+public interface ParseRepository extends JpaRepository<ParseEntity,Long> {
 }
